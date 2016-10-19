4e6f6981ae64fb16eedf24787c82bffc96a288bd
Linh Erickson <linherickson@Linhs-MacBook-Air.local>
2016-10-18 21:43:56 -0700

    updating file

---
+++ functions.py
@@ -0,0 +1,32 @@
+def hello_name(name):
+	return "hello, ", name
+partner_name = "Daniel"
+print hello_name(partner_name)
+
+
+def add(num1, num2):
+	return num1 + num2
+
+def subtract(num1, num2):
+	return num1 - num2
+
+def multiply(num1,num2):
+	return num1 * num2
+
+def divide(num1, num2):
+	return num1 / num2
+
+def modulo(num1, num2):
+	return num1 % num2
+
+def power(base, exponent):
+	return base ** exponent
+
+def square(num):
+	return num ** 2
+
+
+print modulo(3.0,4) / multiply(9,9)
+
+
+
