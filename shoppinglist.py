69ba1ae299493b61133b5192e64e97cbfbc460b5
Linh Erickson <linherickson@Linhs-MacBook-Air.local>
2016-10-28 14:12:31 -0700

    editted

--- shoppinglist.py
+++ shoppinglist.py
@@ -22,21 +22,46 @@ def remove_item():
 		shoppinglist.remove(item)
 		print shoppinglist
 		updated_list()
-
 	else: 
 		print item + " is not in the list"
 		updated_list()
 
-def main():
-	add_item()
-	add_item()
-	add_item()
-	add_item()
-	remove_item()
 
+def menu():
+	print "0 - Main Menu"
+	print "1 - Show current list."
+	print "2 - Add an item to your shopping list."
+	
+
+def main():	
+	while(True):
+		menu()
+		option = int(raw_input("Which option would you like to choose?"))
+		if option == 2:
+			global shoppinglist
+			print "Type exit to exit"
+			item = raw_input("What do you want to add into your list?").lower()
+			if (item == "exit"):
+				break
+			elif item in shoppinglist:
+				print "Item has already been added. Please add something else!"
+			else:
+				shoppinglist.append(item)
+				print shoppinglist
+				updated_list()
+			
+		elif (option == 1):
+			updated_list()
+		else:
+			print "This is your updated shopping list"
+			updated_list()
+	print "This is your updated shopping list"
+	updated_list()
 
 if __name__ == '__main__':
     	main()
+
+
 
 
 
